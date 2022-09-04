359e0555da3acdb394fe7173b214890407bc82b3
samay <shrigadiwar.samay@gmail.com>
2022-09-04 22:14:03 +0300

    Add Problem for if the given number is armstrong number

---
+++ Python/ArmstrongNumber.py
@@ -0,0 +1,45 @@
+"""
+Find if the given number is armstrong number
+
+    I/P: 153
+    O/P: True
+    Explanation:
+        153:    1*1*1 + 5*5*5 + 3*3*3 = 153
+
+    I/P: 256
+    O/P: False
+    Explanation:
+        153:    2*2*2 + 5*5*5 + 3*3*3 = 160
+
+    I/P: 1634
+    O/P: True
+    Explanation:
+        1634:    1*1*1*1 + 6*6*6*6 + 3*3*3*3 + 4*4*4*4 = 1634
+"""
+
+
+def armstrong(int_val):
+    if not int_val:
+        return
+    val = [x for x in str(int_val)]
+    number_length = len(str(int_val))
+    sum = 0
+    for each in val:
+        # We can use pow as well
+        # sum += pow(int(each), number_length)
+        sum += find_power_of_three(int(each), number_length)
+    if sum == int_val:
+        return True
+    else:
+        return False
+
+
+def find_power_of_three(base, power):
+    x = 1
+    for i in range(power):
+        x *= base
+    return x
+
+
+if __name__ == "__main__":
+    print(armstrong(1634))
