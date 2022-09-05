959c6a37def983f31aaa0ae61c66b91748b3382a
samay <shrigadiwar.samay@gmail.com>
2022-09-05 13:07:03 +0300

    Update Problem FirstNonRepeatingCharacter.py to add driver program and docstring

--- Python/FirstNonRepeatingCharacter.py
+++ Python/FirstNonRepeatingCharacter.py
@@ -1,9 +1,14 @@
-#First Non repeating Character in a given String
-'''
-In Given String a = aaabccd
-first Non repeating Character is b
-'''
-def firstNonRepeatingCharacter(s):
+"""
+Find the non repeating character in a given string
+    I/P: s = aaabccd
+    O/P: b
+
+    I/P: s = aaaacaaabbacdaddczaa
+    O/P: z
+"""
+
+
+def non_repeating_char(s):
     freq = {}
     for each in s:        
         if each in freq.keys():
@@ -17,4 +22,7 @@ def firstNonRepeatingCharacter(s):
             break
     return ans
 
-print(firstNonRepeatingCharacter("aaaabaaabbacdaddczaa"))
+
+if __name__ == "__main__":
+    string_val = "aaaabaaabbaddaczaa"
+    print(non_repeating_char(string_val))
