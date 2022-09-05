72dddd11a077f754e7fb0f49b2c478e79301b840
samay <shrigadiwar.samay@gmail.com>
2022-09-05 11:23:30 +0300

    Update Problem for reverse linked list

--- Python/ReverseLinkedList.py
+++ Python/ReverseLinkedList.py
@@ -1,15 +1,57 @@
 """
 Reverse a Linked List
+        a           b           c          d          e          f
+    [None, 1]---->[a, 3]---->[b, 5]---->[c, 4]---->[d, 2]---->[e, 1]
+
+       f           e           d           c         b          a
+    [None, 1]---->[f, 2]---->[e, 4]---->[d, 5]---->[c, 3]---->[b, 1]
 """
 
-def reverse(head):
-    current = head
-    prev = None
-    while current is not None:
-        next = current.next
-        current.next = prev
-        prev = current
-        current = next
-    head = prev
-    print(head)
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Function to reverse the linked list
+    def reverse_link_list(self):
+        prev = None
+        current = self.head
+
+        while current is not None:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        self.head = prev
+
+    # Function to insert a new node at the beginning
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    # Utility function to print the linked LinkedList
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+
+
+if __name__ == "__main__":
+    llist = LinkedList()
+    llist.push(20)
+    llist.push(4)
+    llist.push(15)
+    llist.push(85)
+    print("LinkedList:")
+    llist.print_list()
+    llist.reverse_link_list()
+    print("\nReversed LinkedList:")
+    llist.print_list()
