24565eb973c3be2685cca4faf0b1203c11f4f01e
samay <shrigadiwar.samay@gmail.com>
2022-09-16 00:56:08 +0300

    Update MinCents.py to add more explanation in the problem

--- Python/MinCents.py
+++ Python/MinCents.py
@@ -15,6 +15,24 @@ Count = 5
 
 
 def count_cents(cents):
+    """
+    25
+    coin_count = 243//25 = 0 + 9 = 9
+    cents = 243 % 25 = 18
+
+    10
+    coin_count = 9 + 18//10 = 9 + 1 = 10
+    cents = 18 % 10 = 8
+
+    5
+    coin_count = 11 + 8//5 = 11+1 = 11
+    cents = 8 % 5 = 3
+
+    1
+    coin_count = 11 + 3//1 = 14
+    cents = 3 % 1 = 0
+
+    """
     if cents == 0:
         return 0
     coins = [25, 10, 5, 1]
